31938e372d303b4c0ed95fcdd59c5779595ea1c9
Babu Nair <babunairofficial@gmail.com>
2025-09-24 17:05:09 +0530

    connection to database

--- sqlClass/index.js
+++ sqlClass/index.js
@@ -1,16 +1,33 @@
 // CJS
 const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'shaktimaan_app',
+  password: '' //enter password of connection created in mysql
+});
+
+//a query
+try {
+  connection.query("SHOW TABLES", (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  });
+} catch (err) {
+  console.log(err);
+}
+
+// end connection
+connection.end();
 
 let createRandomUser = () => {
   return {
     userId: faker.string.uuid(),
     username: faker.internet.username(),
     email: faker.internet.email(),
-    avatar: faker.image.avatar(),
     password: faker.internet.password(),
-    birthdate: faker.date.birthdate(),
-    registeredAt: faker.date.past(),
   };
 }
-
-console.log(createRandomUser());
