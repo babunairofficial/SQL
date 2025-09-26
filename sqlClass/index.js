e37269d6fc0f6115f62ffbfe8ca2570b38717711
Babu Nair <babunairofficial@gmail.com>
2025-09-26 10:07:03 +0530

    manually adding mulitple data as array of arrays

--- sqlClass/index.js
+++ sqlClass/index.js
@@ -22,11 +22,31 @@ try {
 
 //Inserting New Data - using placeholders
 
-let q = "INSERT INTO user (id, username, email, password) VALUES (?, ?, ?, ?)"; //storing the query in a variable
-let user = ["1", "1_newsuperhero", "nsh@gmail.com", "nsh"]; //array to store the placeholder values
+// let q = "INSERT INTO user (id, username, email, password) VALUES (?, ?, ?, ?)"; //storing the query in a variable
+// let user = ["1", "1_newsuperhero", "nsh@gmail.com", "nsh"]; //array to store the placeholder values
+
+// try {
+//   connection.query(q, user, (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+// } catch (err) {
+//   console.log(err);
+// }
+
+let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+//bulk insert query using a placeholder for multiple columns
+
+//user data as an array of arrays
+let users = [
+  ["2", "2_newsuperhero", "nshb@gmail.com", "nshb"],
+  ["3", "3_newsuperhero", "nshc@gmail.com", "nshc"]
+];
+
 
 try {
-  connection.query(q, user, (err, result) => {
+  // query with parameterized input
+  connection.query(q, [users], (err, result) => { // users data to be entered within square brackets
     if (err) throw err;
     console.log(result);
   });
