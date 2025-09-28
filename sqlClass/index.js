a098feeb61d3166f7f13880ab2d5acee33e62cba
Babu Nair <babunairofficial@gmail.com>
2025-09-29 00:37:52 +0530

    routing the sql data - templating and routing

--- sqlClass/index.js
+++ sqlClass/index.js
@@ -3,6 +3,11 @@ const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
 
 // Create the connection to database
 const connection = mysql.createConnection({
@@ -12,18 +17,7 @@ const connection = mysql.createConnection({
   password: '' //enter password of connection created in mysql
 });
 
-// try {
-//   // query with parameterized input
-//   connection.query(q, [data], (err, result) => { // users data to be entered within square brackets
-//     if (err) throw err;
-//     console.log(result);
-//   });
-// } catch (err) {
-//   console.log(err);
-// }
 
-// // end connection
-// connection.end();
 
 app.get("/", (req, res) => {
   let q = `SELECT count(*) FROM user`;
@@ -31,8 +25,8 @@ app.get("/", (req, res) => {
     // query with parameterized input
     connection.query(q, (err, result) => { // users data to be entered within square brackets
       if (err) throw err;
-      console.log(result);
-      res.send(result);
+      let count = result[0]["count(*)"];
+      res.render("home.ejs", { count });
     });
   } catch (err) {
     console.log(err);
