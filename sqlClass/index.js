64be9e3286c11a3f56ab8a41018b729583a4b4b7
Babu Nair <babunairofficial@gmail.com>
2025-10-04 11:52:44 +0530

    UPDATE route to update database

--- sqlClass/index.js
+++ sqlClass/index.js
@@ -4,7 +4,10 @@ const mysql = require('mysql2');
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -51,8 +54,49 @@ app.get("/user", (req, res) => {
   }
 });
 
-app.listen("8080", () => {
-  console.log("server is listening to port 8080");
+
+//Edit Route
+app.get("/user/:id/edit", (req, res) => {
+  let { id } = req.params;
+  console.log(id);
+  let q = `SELECT * FROM user WHERE id='${id}'`;
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let user = result[0];
+      res.render("edit.ejs", {user});
+    });
+  } catch (err) {
+    console.log(err);
+    res.send("database error");
+  }
+});
+
+
+//UPDATE route
+app.patch("/user/:id", (req, res) => {
+  let { id } = req.params;
+  let {password: formPass, username: newUsername} = req.body;
+   
+  let q = `SELECT * FROM user WHERE id='${id}'`;
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let user = result[0];
+      if(formPass != user.password) {
+        res.send("Wrong Password");
+      } else {
+        let q2 = `UPDATE user SET username='${newUsername}' WHERE id='${id}'`;
+        connection.query(q2, (err, result) => {
+          if (err) throw err;
+          res.redirect("/user");
+        })
+      }    
+    });
+  } catch (err) {
+    console.log(err);
+    res.send("database error");
+  }
 });
 
 app.listen("8080", () => {
